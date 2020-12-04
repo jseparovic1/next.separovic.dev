138aab24103920b999fffadef5ddfd6b4a222f9a
Jurica Šeparović <jurica.separovic@gmail.com>
2020-12-04 16:19:20 +0100

    Modify header blink text.

--- components/HeaderBlinkText.js
+++ components/HeaderBlinkText.js
@@ -3,7 +3,7 @@ export default function HeaderBlinkText({title}) {
         <a className="flex items-center text-2xl lg:text-3xl pt-6 leading-none" href="/">
             <span className="text-red-700 font-extrabold">~</span>
             <h1 className="ml-1 text-blue-100 font-bold tracking-tight">{title}</h1>
-            <div className="ml-2 mt-2 w-3 h-5 lg:h-6 bg-gray-100 animate-blink"/>
+            <div className="ml-2 mt-2 w-2 lg:w-3 h-4 lg:h-6 bg-gray-100 animate-blink"/>
         </a>
     );
 }
--- components/Post.js
+++ components/Post.js
@@ -11,7 +11,7 @@ export default function Post({meta, children}) {
                 <meta property="og:description" content={meta.description}/>
                 <meta name="description" content={meta.description}/>
             </Head>
-            <div className="px-4 md:px-10 pt-8 lg:pt-12 mx-auto max-w-4xl">
+            <div className="px-4 md:px-10 pt-8 mx-auto max-w-4xl">
                 <h1 className="text-5xl lg:text-6xl font-extrabold tracking-tight leading-none">
                     {meta.title}
                 </h1>
