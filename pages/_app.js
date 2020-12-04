aeaf043843e5c828f6defe8fb4fc180f4ba87ba1
Jurica Šeparović <jurica.separovic@gmail.com>
2020-12-04 16:12:39 +0100

    Add head component back.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import '../css/tailwind.css';
 import React from "react";
-import Header from "../components/Header";
 import Head from "next/head";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
+import HeaderBlinkText from "../components/HeaderBlinkText";
 
 export default function App({Component, pageProps}) {
     return (
@@ -13,7 +14,7 @@ export default function App({Component, pageProps}) {
             <div className="relative antialiased fonts-sans min-h-screen bg-gray-900 text-gray-200">
                 <div className="absolute w-full h-1 bg-red-700"/>
                 <Header
-                    left=<HeaderBlinkText title="separovic.dev"/>
+                    left={<HeaderBlinkText title="separovic.dev"/>}
                 />
                 <main>
                     <Component {...pageProps}/>
