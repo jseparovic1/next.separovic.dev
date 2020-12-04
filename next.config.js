5de11ad1ab610e3faf0ebe4805e847ab8b9f19b2
Jurica Šeparović <jurica.separovic@gmail.com>
2020-12-04 17:22:59 +0100

    Add syntax highlighting and remove unused packages.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
+const rehypePrism = require('@mapbox/rehype-prism');
+const rehypeSlug = require('rehype-slug');
+const rehypeHeadings = require('rehype-autolink-headings');
+
 const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  remarkPlugins: [],
-  rehypePlugins: [
-      require('@mapbox/rehype-prism'),
-      require('rehype-slug')
-  ]
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [
+            rehypePrism,
+            rehypeSlug,
+            rehypeHeadings,
+        ]
+    }
 })
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'],
+    pageExtensions: ['js', 'jsx', 'mdx'],
 })
